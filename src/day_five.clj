b59aad2f5e4d4f2e8b57e3eb7653f84e69eb22ca
Grant Horner <h.grant.horner@gmail.com>
2021-08-21 20:44:25 -0400

    Finished part one day 5

--- src/day_five.clj
+++ src/day_five.clj
@@ -1,18 +1,67 @@
-(ns day-five)
+(ns day-five
+  (:require [clojure.string :refer [split-lines]]
+            [clojure.pprint :refer [pprint]]))
 
 (def sample "FBFBBFFRLR")
+(def sample2 "BFFFBBFRRR")
+(def sample3 "FFFBBBFRRR")
 
 (defn ceil
   [x]
   (int (Math/ceil x)))
 
-(reduce
- (fn [{mn :min mx :max} char]
-   (if (= char \F)
-    {:min (+ (ceil (/ (- mx mn) 2)) mn) :max mx}
-    {:min mn :max ()}))
- sample)
+(defn plane-row
+  [code]
+  (reduce
+    (fn [{mn :min mx :max} char]
+      (if (= char \F)
+        {:min mn :max (+ (int (/ (- mx mn) 2)) mn)}
+        {:min (+ (ceil (/ (- mx mn) 2)) mn) :max mx}))
+    {:min 0 :max 127}
+    (take 7 code)))
 
-(+ (int (/ (- 0 127) 2)) 127)
+(defn plane-col
+  [code]
+  (reduce
+    (fn [{mn :min mx :max} char]
+      (if (= char \L)
+        {:min mn :max (+ (int (/ (- mx mn) 2)) mn)}
+        {:min (+ (ceil (/ (- mx mn) 2)) mn) :max mx}))
+    {:min 0 :max 7}
+    (reverse (take 3 (reverse code)))))
 
-(+ (int (/ (- 127 0) 2)) 0)
+(plane-col "BBFFBBFRLL")
+
+(defn plane-id
+  [row col]
+  (+ (* row 8) col))
+
+(defn plane-info
+  [code]
+  (let [row (:min (plane-row code))
+        col (:min (plane-col code))]
+    {:row row :col col :id (plane-id row col)}))
+
+(defn puzzle-one
+  [input]
+  (let [f (comp
+            (map plane-info)
+            (map :id))]
+    (apply max (into [] f input))))
+
+
+(comment
+  (def input (-> "src/day_five.txt" slurp split-lines))
+  (apply max (map plane-info input))
+  (puzzle-one input)
+  (plane-info sample2)
+  (plane-info sample3)
+  (plane-info "BBFFBBFRLL")
+  (plane-col sample)
+  (plane-col sample2)
+  (plane-col sample3)
+  (plane-col sample3)
+  (plane-col "BBFFBBFRLL")
+  (plane-row sample)
+  (plane-row sample2)
+  (plane-row sample3))
