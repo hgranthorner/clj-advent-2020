e5907be4027c85e56e4dcd4019be9004e21a73d0
Grant Horner <h.grant.horner@gmail.com>
2021-08-25 17:50:45 -0400

    Finished day 7

--- src/day_seven.clj
+++ src/day_seven.clj
@@ -1,7 +1,8 @@
 (ns day-seven
- (:require [clojure.string :as s]
-           [clojure.pprint :refer [pprint]]
-           [clojure.set :refer [union]]))
+  (:require [clojure.string :as s]
+            [clojure.pprint :refer [pprint]]
+            [clojure.set :refer [union]
+             [clojure.string :as str]]))
 
 (def input "light red bags contain 1 bright white bag, 2 muted yellow bags.
 dark orange bags contain 3 bright white bags, 4 muted yellow bags.
@@ -17,14 +18,14 @@ dotted black bags contain no other bags.")
 
 (def clean-str
   (comp
-    (map #(s/replace % "bags" "bag"))
-    (map #(s/replace % "." ""))))
+   (map #(s/replace % "bags" "bag"))
+   (map #(s/replace % "." ""))))
 
 (defn try-parse-int
   [x]
   (try
     (Integer/parseInt x)
-    (catch Exception _ false)))
+    (catch Exception _ nil)))
 
 (defn str->bag-amount-map
   [s]
@@ -33,21 +34,21 @@ dotted black bags contain no other bags.")
                 first
                 try-parse-int)
         name (as-> s x
-                 (s/split x #" ")
-                 (rest x)
-                 (s/join " " x))]
+               (s/split x #" ")
+               (rest x)
+               (s/join " " x))]
     {name num}))
 
 (defn arrs->bag-map
   [s]
   (apply
-    merge
-    (map
-      (fn [[k vs]]
-        {k (->> vs
-                (map str->bag-amount-map)
-                (filter #(nil? (get % "other bag")))
-                (into []))}) s)))
+   merge
+   (map
+    (fn [[k vs]]
+      {k (->> vs
+              (map str->bag-amount-map)
+              (filter #(nil? (get % "other bag")))
+              (into []))}) s)))
 
 (defn can-hold
   [xs bag]
@@ -58,26 +59,22 @@ dotted black bags contain no other bags.")
        (filter some?)
        set))
 
-; (can-hold x "dotted black bag")
+                                        ; (can-hold x "dotted black bag")
 
 (defn puzzle-one
   [input]
   (let [bag-map (->> input
-                        s/split-lines
-                        (sequence clean-str)
-                        (map #(s/split % #" contain "))
-                        (map (fn [[k vs]] [k (vec (s/split vs #", "))]))
-                        arrs->bag-map)]
+                     s/split-lines
+                     (sequence clean-str)
+                     (map #(s/split % #" contain "))
+                     (map (fn [[k vs]] [k (vec (s/split vs #", "))]))
+                     arrs->bag-map)]
     (loop [bags (can-hold bag-map "shiny gold bag")]
       (let [new-bags (apply union (cons bags (map #(can-hold bag-map %) bags)))]
         (if (= bags new-bags)
           bags
           (recur new-bags))))))
 
-; {bag [{b1 1} {b2 3}]}
-; [bag] 0
-; [b1 b2] 4
-;
 (declare x)
 
 (defn- containing-bags
@@ -91,16 +88,16 @@ dotted black bags contain no other bags.")
         (recur next-bags (+ total-bags amount)))
       total-bags)))
 
-; (containing-bags x "dotted-black-bag")
+                                        ; (containing-bags x "dotted-black-bag")
 
 (defn puzzle-two
   [input]
   (let [bag-map (->> input
-                         s/split-lines
-                         (sequence clean-str)
-                         (map #(s/split % #" contain "))
-                         (map (fn [[k vs]] [k (vec (s/split vs #", "))]))
-                         arrs->bag-map)]
+                     s/split-lines
+                     (sequence clean-str)
+                     (map #(s/split % #" contain "))
+                     (map (fn [[k vs]] [k (vec (s/split vs #", "))]))
+                     arrs->bag-map)]
     (def bm bag-map)
     (loop [bags-to-check ["shiny gold bag"]
            total 0]
@@ -123,14 +120,58 @@ dotted black bags contain no other bags.")
                         :children [{:name "faded blue bag" :val 5}
                                    {:name "dotted black bag" :val 6}]}]})
 
+(defn bag-string->map [s]
+  (let [arrs (s/split s #" ")
+        val (try-parse-int (first arrs))]
+    {(s/join " " (rest arrs)) val}))
 
+(def x (-> (slurp "src/day_seven.txt")
+           (s/replace #"bags" "bag")
+           (s/replace #"\." "")
+           (s/split-lines)
+           (->> (map #(s/split % #" contain "))
+                (map (fn [[k vs]] [k (apply merge
+                                            (map bag-string->map (s/split vs #", ")))]))
+                (into {}))))
 
+(defn find-contained-bags
+  [s bag-map]
+  (let [bags (get bag-map s)
+        bag-names (keys bags)
+        total (reduce + (vals bags))]
+    [total bag-names]))
+
+(find-contained-bags "shiny gold bag" x)
+
+(defn traverse-bags
+  [bag mult total bag-map]
+  (let [next-bags (get bag-map bag)]
+    (if (not= (first (keys next-bags)) "other bag")
+      (+ total
+         (reduce
+          +
+          (for [[next-bag-name next-bag-val] next-bags
+                :let [next-mult (* mult next-bag-val)
+                      ret-val (traverse-bags next-bag-name next-mult next-mult bag-map)]]
+            ret-val)))
+      total)))
+
+(traverse-bags "shiny gold bag" 1 0 x)
+                                        ; -> traverse-pags "dark olive bag" 1 1 x
+                                        ; -> traverse-bags "faded blue bag" 3 4 x
+                                        ; -> faded blue bag: 4
+
+                                        ; -> traverse-bags dotted black bag 4 5
+                                        ; -> 5
+                                        ; -> dotted black bag: 5
+                                        ; dark olive bag: 9
 (comment
+  (map bag-string->map ["1 bright white bag" "2 muted yellow bag"])
   (puzzle-two input)
 
   (puzzle-one input)
   (count (puzzle-one (slurp "src/day_seven.txt")))
-  ; Find which containers can hold a specific bag
+                                        ; Find which containers can hold a specific bag
   (map (fn [[container bags]]
          (if (some #{"dotted black bag"} (flatten (map keys bags)))
            container))
@@ -138,10 +179,10 @@ dotted black bags contain no other bags.")
   (puzzle-one input)
 
   (def y (->> input
-             s/split-lines
-             (sequence clean-str)
-             (map #(s/split % #" contain "))
-             first))
+              s/split-lines
+              (sequence clean-str)
+              (map #(s/split % #" contain "))
+              first))
 
   {:name (first y)
    :val 1
@@ -152,9 +193,9 @@ dotted black bags contain no other bags.")
                       {:name k :val v}) (s/split (second y) #", ")))}
 
   (def x (->> input
-           s/split-lines
-           (sequence clean-str)
-           (map #(s/split % #" contain "))
-           (map (fn [[k vs]] [k (vec (s/split vs #", "))]))
-           arrs->bag-map))
- (comment))
+              s/split-lines
+              (sequence clean-str)
+              (map #(s/split % #" contain "))
+              (map (fn [[k vs]] [k (vec (s/split vs #", "))]))
+              arrs->bag-map))
+  (comment))
